bc6947b91a32f15370a332b91a4c9136dc11a740
unknown <190594990+SecByteX@users.noreply.github.com>
2025-07-06 23:45:51 +0300

    feat: add input validation for IP/domain

--- python/scanforce.py
+++ python/scanforce.py
@@ -1,11 +1,18 @@
 import socket 
 
-print("Welcome to port scanner application...")
+print("scan force - the power of scanning")
+
 ip = input("target:")
 
+try:
+    ip = socket.gethostbyname(ip)
+except socket.gaierror:
+    print("[-] Invalid IP or domain name")
+    exit()
 
 
-print("Starting TCP Connect Scan")
+
+print("Starting TCP Connect Scan , 0-1024")
 for port in range(0,1025):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.settimeout(0.5)
@@ -15,9 +22,5 @@ for port in range(0,1025):
     except :
         pass
     finally:
-        client.close()
-        
-    
-
-    
+        client.close()    
 
