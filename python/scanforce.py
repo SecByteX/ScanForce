4007bb44e7230840e1a5021ecf3a0c93469b2d26
SecByteX <190594990+SecByteX@users.noreply.github.com>
2025-07-07 23:07:17 +0300

    feat: add argparse

--- python/scanforce.py
+++ python/scanforce.py
@@ -1,19 +1,27 @@
 import socket 
+import argparse
 
 print("scan force - the power of scanning")
 
-ip = input("target:")
+parser = argparse.ArgumentParser(description="TCP Connect Port Scanner")
+parser.add_argument("--target",required=True,help="Target IP/Domain")
+parser.add_argument("--ports",default="1-1024",help="Port range (e.g 20-40)")
+args = parser.parse_args()
 
 try:
-    ip = socket.gethostbyname(ip)
+    ip = socket.gethostbyname(args.target)
 except socket.gaierror:
     print("[-] Invalid IP or domain name")
     exit()
 
 
+try :
+    start_port , end_port = map(int, args.ports.split("-"))
+except:
+    print("[-] Invalid Port Range (e.g. 20-40)")
+    exit()
 
-print("Starting TCP Connect Scan , 0-1024")
-for port in range(0,1025):
+for port in range(start_port,end_port+1):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.settimeout(0.5)
     try:
